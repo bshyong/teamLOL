ec35483a7d1de4a553d7c46f67f590b25fa8c17c
Ben Shyong <likeachild@gmail.com>
2011-02-02 19:32:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpennRails::Application.config.secret_token = '76e98bd656cd13283f1c5782e3802b08c97e2c4a498cfa030794294758bd6d4c2e401a6ee1fe19e84e01d858100fc7f4cb7046c6a82409946b07fdbd7547930e'
